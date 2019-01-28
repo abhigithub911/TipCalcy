6e51fa9e3ba261975f99c783f1eb88ab230034df
Abhishek Konduri <abhishekkonduri911@gmail.com>
2019-01-28 00:43:22 -0800

    Initial Commit

---
+++ TipCalcy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalcy
+//
+//  Created by Abhishek Konduri on 1/28/19.
+//  Copyright © 2019 Abhishek Konduri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
